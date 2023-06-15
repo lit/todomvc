26d84d55540a565181c629d70dd7df6166cb74ce
Peter Burns <rictic@google.com>
2023-06-15 11:21:35 -0700

    Clean up updateOnEvent a bit

--- examples/lit/src/lib/utils.ts
+++ examples/lit/src/lib/utils.ts
@@ -1,21 +1,35 @@
 import { ReactiveElement } from "lit";
 
+/**
+ * A property decorator that subscribes to an event on the property value and
+ * calls `requestUpdate` when the event fires.
+ *
+ * If we were using this outside of just this one app we'd use the type system
+ * to enforce that the property value is an `EventTarget`.
+ */
 export const updateOnEvent =
-	(_eventName: string) => (target: unknown, propertyKey: string) => {
+	(eventName: string) => (target: ReactiveElement, propertyKey: string) => {
 		const descriptor = Object.getOwnPropertyDescriptor(target, propertyKey)!;
-		console.log({ target, propertyKey, descriptor });
+		if (
+			descriptor == null ||
+			descriptor.get == null ||
+			descriptor.set == null
+		) {
+			throw new Error(
+				`updateOnEvent should only be called on an accessor, ` +
+					`but ${propertyKey} did not have both a getter and setter`
+			);
+		}
 		const { get, set } = descriptor;
-    const newDescriptor = {
-      ...descriptor,
-      set(this: ReactiveElement, v: EventTarget) {
-        console.log('updateOnEvent set');
-        const listener = ((this as any).__updateOnEventListener ??= () =>
-          this.requestUpdate());
-        const oldValue = get!.call(this);
-        oldValue?.removeEventListener?.(_eventName, listener);
-        v?.addEventListener?.(_eventName, listener);
-        return set!.call(this, v);  
-      }
-    };
-    Object.defineProperty(target, propertyKey, newDescriptor);
+		const requestUpdate = () => target.requestUpdate();
+		const newDescriptor = {
+			...descriptor,
+			set(this: ReactiveElement, v: EventTarget) {
+				const oldValue = get!.call(this);
+				oldValue?.removeEventListener?.(eventName, requestUpdate);
+				v?.addEventListener?.(eventName, requestUpdate);
+				return set!.call(this, v);
+			},
+		};
+		Object.defineProperty(target, propertyKey, newDescriptor);
 	};
