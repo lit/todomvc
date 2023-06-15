cfe76e7d98eacc00409bb7b1a00300ce1f9a15d4
Peter Burns <rictic@google.com>
2023-06-15 11:23:33 -0700

    Read filter from URL

--- examples/lit/src/lib/todo-app.ts
+++ examples/lit/src/lib/todo-app.ts
@@ -1,9 +1,10 @@
 import { LitElement, html, css } from "lit";
 import { customElement } from "lit/decorators/custom-element.js";
 import { property } from "lit/decorators/property.js";
+import { state } from "lit/decorators/state.js";
 
 import { todoStyles } from "./todo.css.js";
-import { TodoFilter, Todos } from "./todos.js";
+import { TodoFilter, Todos, isTodoFilter } from "./todos.js";
 
 import "./todo-list.js";
 import "./todo-form.js";
@@ -14,7 +15,7 @@ import {
 	ToggleAllTodoEvent,
 	EditTodoEvent,
 } from "./events.js";
-// import { updateOnEvent } from "./utils.js";
+import { updateOnEvent } from "./utils.js";
 
 @customElement("todo-app")
 export class TodoApp extends LitElement {
@@ -27,11 +28,12 @@ export class TodoApp extends LitElement {
 		`,
 	];
 
-  // @updateOnEvent('change')
+	@updateOnEvent("change")
+	@state()
 	todoList = new Todos();
 
 	@property()
-	filter: TodoFilter = "all";
+	filter: TodoFilter = this.#filterFromUrl();
 
 	constructor() {
 		super();
@@ -41,14 +43,30 @@ export class TodoApp extends LitElement {
 		this.addEventListener(ToggleAllTodoEvent.eventName, this.#onToggleAll);
 	}
 
+	override connectedCallback(): void {
+		super.connectedCallback();
+		window.addEventListener("hashchange", this.#onHashChange);
+	}
+
+	override disconnectedCallback(): void {
+		super.disconnectedCallback();
+		window.removeEventListener("hashchange", this.#onHashChange);
+	}
+
 	override render() {
 		return html` <section class="todoapp">
 			<header class="header">
 				<h1>todos</h1>
-				<todo-form .todoList=${this.todoList}></todo-form>
+				<todo-form
+					.todoList=${this.todoList}
+					.filter=${this.filter}
+				></todo-form>
 			</header>
 			<section class="main">
-				<todo-list .todoList=${this.todoList}></todo-list>
+				<todo-list
+					.todoList=${this.todoList}
+					.filter=${this.filter}
+				></todo-list>
 			</section>
 			<todo-footer .todoList=${this.todoList} .selectedFilter=${this.filter}>
 			</todo-footer>
@@ -70,11 +88,22 @@ export class TodoApp extends LitElement {
 	#onToggleAll = (_e: ToggleAllTodoEvent) => {
 		this.todoList.toggleAll();
 	};
-}
 
+	#onHashChange = () => {
+		this.filter = this.#filterFromUrl();
+	};
+
+	#filterFromUrl() {
+		let filter = /#\/(.*)/.exec(window.location.hash)?.[1];
+		if (isTodoFilter(filter)) {
+			return filter;
+		}
+		return "all";
+	}
+}
 
 declare global {
 	interface HTMLElementTagNameMap {
-		"todo-app": TodoApp
+		"todo-app": TodoApp;
 	}
 }
